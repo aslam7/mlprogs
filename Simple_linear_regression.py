5503e55c1fd6bac554c07565442be7ec0098aba4
aslam7 <aslam.iiitb407@gmail.com>
2017-03-11 23:46:26 +0530

    Add files via upload

---
+++ Simple_linear_regression.py
@@ -0,0 +1,41 @@
+def mean(values):
+	return sum(values)/float(len(values))
+
+def variance(values, mean):
+	return sum([(x-mean)**2 for x in values])
+
+def covariance(x, mean_x, y, mean_y):
+	covar = 0.0
+	for i in range(len(x)):
+		covar+=(x[i]-mean_x) * (y[i]-mean_y)
+	return covar
+def coefficients(dataset):
+	x = []
+	y = []
+
+	for line in dataset:
+		xi, yi = map(float, line.split(','))
+		if xi<4:
+			x.append(xi)
+			y.append(yi)
+
+	dataset.close()								
+	
+	x_mean, y_mean = mean(x), mean(y)
+
+	b1 = covariance(x, x_mean, y, y_mean)/variance(x, x_mean)
+	b0 = y_mean-b1*x_mean
+
+	return [b0, b1]
+
+dataset = open('trainingdata.txt')
+
+b0, b1 = coefficients(dataset)
+
+n=float(raw_input())
+if(n<4):
+	print "%.2f" % (b0+b1*n)
+else:
+	print 8
+
+
